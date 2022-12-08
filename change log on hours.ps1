4653408745b2f703c0dacc794292668a50c85bfb
A-Cardwell <119899230+A-Cardwell@users.noreply.github.com>
2022-12-08 12:44:01 -0800

    Add files via upload

--- change log on hours.ps1
+++ change log on hours.ps1
@@ -1,4 +1,11 @@
-﻿$template_user='clex'
+﻿<# 
+.Synopsis 
+Used for changing log on hours to multiple USers in an OU.
+.Description
+
+#>
+
+$template_user='clex'
 $template_hours= Get-ADUser -Identity $template_user -properties logonHours
 $OU = Get-ADOrganizationalUnit -filter 'name -like "_TempNEWRecruits"' 
 Get-aduser  -Filter * -SearchBase $OU |foreach {Set-ADUSer $_.samaccountname -Replace @{logonHours = $template_hours.logonHours} }
