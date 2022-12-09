bd0fad2998c20059912c508c8a4d4ac9019a82f3
A-Cardwell <119899230+A-Cardwell@users.noreply.github.com>
2022-12-08 23:00:21 -0800

    Add files via upload

--- Initialize Raw Disks and to Partition and Format Volumes.ps1
+++ Initialize Raw Disks and to Partition and Format Volumes.ps1
@@ -1,4 +1,14 @@
-﻿Get-Disk |
+﻿<# 
+.synopsis
+Initialize raw disks, partition, and format volumes
+.Description
+Currently script works with single drives perfectly. Keep in mind if you have
+multiple raw drives, the script will turn them all on and format. 
+If you dont care about that then you need to worry about the newfilesystemlabel.
+The newfilesystemlabel string will be for all disk drives and will not assign disk numbers
+currently trying to figure out this problem. #>
+
+Get-Disk |
 
 Where partitionstyle -eq ‘raw’ |
 
--- change log on hours.ps1
+++ change log on hours.ps1
@@ -1,11 +1,12 @@
 ﻿<# 
 .Synopsis 
 Used for changing log on hours to multiple USers in an OU.
-.Description
-
 #>
 
 $template_user='clex'
 $template_hours= Get-ADUser -Identity $template_user -properties logonHours
 $OU = Get-ADOrganizationalUnit -filter 'name -like "_TempNEWRecruits"' 
 Get-aduser  -Filter * -SearchBase $OU |foreach {Set-ADUSer $_.samaccountname -Replace @{logonHours = $template_hours.logonHours} }
+
+#PROBLEM I had at first was the foreach loop actully get every user in the OU. Realized that I needed to change the get-roganizationlunit 
+# into Get-aduser and filter. Also had to make $OU variable#
